f6937f1851cb010d04d9e62779c0145da23c4745
Sarah <asarahali00@gmail.com>
2021-03-27 16:25:03 -0500

    adding simple html, js, css

--- mockFrontEnd.js
+++ mockFrontEnd.js
@@ -1,80 +1,69 @@
-const target = window.parent;
+window.top.postMessage(
+    JSON.stringify({
+      error: false,
+      message: "Hello World"
+    }),
+    '*'
+  );
 
-console.log(target);
-console.log(target.opener);
+// const squares = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
 
-window.addEventListener('message', event => {
-    // IMPORTANT: check the origin of the data! 
-    if (event.origin.startsWith('http://your-first-site.com')) { 
-        // The data was sent from your site.
-        // Data sent with postMessage is stored in event.data:
-        console.log(event.data); 
-    } else {
-        // The data was NOT sent from your site! 
-        // Be careful! Do not use it. This else branch is
-        // here just for clarity, you usually shouldn't need it.
-        return; 
-    } 
-}); 
+// Game.setupHooks = () => {
+//     const onClick = (e) => {
+//         Game.onMove(e.target.id);
+//         if (Game.gameOver()) Game.onEnd();
+//         else {
+//             Game.SetCurrentPlayer(Game.getNextPlayer());
+//             Game.getValueByID("subtitle").innerHTML = `Current Player: ${Game.currentPlayer.userName}`
+//         }
+//     }
 
-const squares = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+//     squares.forEach((square) => {
+//         Game.getValueByID(`square${square}`).onclick = onClick;
+//     });
+// }
 
-Game.setupHooks = () => {
-    const onClick = (e) => {
-        Game.onMove(e.target.id);
-        if (Game.gameOver()) Game.onEnd();
-        else {
-            Game.SetCurrentPlayer(Game.getNextPlayer());
-            Game.getValueByID("subtitle").innerHTML = `Current Player: ${Game.currentPlayer.userName}`
-        }
-    }
+// Game.onStart = () => {
+//     const PlayerOne = new Player("PlayerOne", { "playerToken": "X" })
+//     const PlayerTwo = new Player("PlayerTwo", { "playerToken": "O" })
 
-    squares.forEach((square) => {
-        Game.getValueByID(`square${square}`).onclick = onClick;
-    });
-}
+//     Game.AddPlayers([PlayerOne, PlayerTwo]);
+//     Game.SetCurrentPlayer(PlayerOne);
 
-Game.onStart = () => {
-    const PlayerOne = new Player("PlayerOne", { "playerToken": "X" })
-    const PlayerTwo = new Player("PlayerTwo", { "playerToken": "O" })
+//     Game.getValueByID("subtitle").innerHTML = `Current Player: ${Game.currentPlayer.userName}`;
+// }
 
-    Game.AddPlayers([PlayerOne, PlayerTwo]);
-    Game.SetCurrentPlayer(PlayerOne);
+// Game.onMove = (clickedElementID) => {
+//     Game.getValueByID(clickedElementID).innerHTML = Game.currentPlayer.properties["playerToken"];
+// }
 
-    Game.getValueByID("subtitle").innerHTML = `Current Player: ${Game.currentPlayer.userName}`;
-}
+// Game.onEnd = () => {
+//     Game.getValueByID("subtitle").innerHTML = `Winning Player: ${Game.currentPlayer.userName}`;
+// }
 
-Game.onMove = (clickedElementID) => {
-    Game.getValueByID(clickedElementID).innerHTML = Game.currentPlayer.properties["playerToken"];
-}
+// Game.getValues = () => {
+//     const values = [];
 
-Game.onEnd = () => {
-    Game.getValueByID("subtitle").innerHTML = `Winning Player: ${Game.currentPlayer.userName}`;
-}
+//     squares.forEach((square) => values.push(Game.getValueByID(`square${square}`).innerHTML));
 
-Game.getValues = () => {
-    const values = [];
+//     return values;
+// }
 
-    squares.forEach((square) => values.push(Game.getValueByID(`square${square}`).innerHTML));
+// Game.gameOver = () => {
+//     const values = Game.getValues();
 
-    return values;
-}
+//     if (
+//         (values[0] != '' && values[0] == values[1] && values[1] == values[2]) ||
+//         (values[0] != '' && values[0] == values[3] && values[3] == values[6]) ||
+//         (values[0] != '' && values[0] == values[4] && values[4] == values[8]) ||
+//         (values[1] != '' && values[1] == values[4] && values[4] == values[7]) ||
+//         (values[2] != '' && values[2] == values[4] && values[4] == values[6]) ||
+//         (values[2] != '' && values[2] == values[5] && values[5] == values[8]) ||
+//         (values[3] != '' && values[3] == values[4] && values[4] == values[5]) ||
+//         (values[6] != '' && values[6] == values[7] && values[7] == values[8])
+//     ) {
+//         return true;
+//     }
 
-Game.gameOver = () => {
-    const values = Game.getValues();
-
-    if (
-        (values[0] != '' && values[0] == values[1] && values[1] == values[2]) ||
-        (values[0] != '' && values[0] == values[3] && values[3] == values[6]) ||
-        (values[0] != '' && values[0] == values[4] && values[4] == values[8]) ||
-        (values[1] != '' && values[1] == values[4] && values[4] == values[7]) ||
-        (values[2] != '' && values[2] == values[4] && values[4] == values[6]) ||
-        (values[2] != '' && values[2] == values[5] && values[5] == values[8]) ||
-        (values[3] != '' && values[3] == values[4] && values[4] == values[5]) ||
-        (values[6] != '' && values[6] == values[7] && values[7] == values[8])
-    ) {
-        return true;
-    }
-
-    return false;
-}
+//     return false;
+// }
