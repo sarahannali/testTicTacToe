e845af064d87a033eeaa98bb7f823ae19cc39363
Sarah <asarahali00@gmail.com>
2021-03-27 16:18:05 -0500

    simplify html

--- mockFrontEnd.js
+++ mockFrontEnd.js
@@ -3,6 +3,20 @@ const target = window.parent;
 console.log(target);
 console.log(target.opener);
 
+window.addEventListener('message', event => {
+    // IMPORTANT: check the origin of the data! 
+    if (event.origin.startsWith('http://your-first-site.com')) { 
+        // The data was sent from your site.
+        // Data sent with postMessage is stored in event.data:
+        console.log(event.data); 
+    } else {
+        // The data was NOT sent from your site! 
+        // Be careful! Do not use it. This else branch is
+        // here just for clarity, you usually shouldn't need it.
+        return; 
+    } 
+}); 
+
 const squares = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
 
 Game.setupHooks = () => {
